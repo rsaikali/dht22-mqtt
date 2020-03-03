446d4da41c3175dea90a407baa64e80e88c84fe7
Roland SAIKALI <roland@saikali.fr>
2020-03-03 21:53:15 +0100

    Fixed 100% CPU use of libgpiod_pulsein

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,23 @@
+###################
+# Build environment to get fresh libgpiod_pulsein
+# See also: https://github.com/adafruit/libgpiod_pulsein/pull/1
+#           https://github.com/adafruit/Adafruit_Blinka/issues/210
+FROM python:3.8-slim-buster AS builder
+
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y libgpiod2 libgpiod-dev git gcc build-essential && \
+    rm -rf /var/lib/apt/lists/*
+
+WORKDIR /opt
+
+RUN git clone https://github.com/michaellass/libgpiod_pulsein.git && \
+    cd /opt/libgpiod_pulsein && \
+    git checkout cpu-fix && \
+    cd src && \
+    make
+
+########################
+# Production environment
 FROM python:3.8-slim-buster
 
 ENV DHT22_PIN 4
@@ -17,6 +37,8 @@ WORKDIR /opt
 COPY requirements.txt /opt/requirements.txt
 
 RUN pip3 install --no-cache-dir -r /opt/requirements.txt
+
+COPY --from=builder /opt/libgpiod_pulsein/src/libgpiod_pulsein /usr/local/lib/python3.8/site-packages/adafruit_blinka/microcontroller/bcm283x/pulseio/libgpiod_pulsein
 
 COPY dht22-mqtt.py /opt/dht22-mqtt.py
 
--- dht22-mqtt.py
+++ dht22-mqtt.py
@@ -11,19 +11,13 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(name)s] %(leveln
 
 # Config from environment (see Dockerfile)
 DHT22_PIN = int(os.getenv('DHT22_PIN', '4'))
-DHT22_CHECK_EVERY = int(os.getenv('DHT22_CHECK_EVERY', 10))
+DHT22_CHECK_EVERY = int(os.getenv('DHT22_CHECK_EVERY', 1))
 MQTT_SERVICE_HOST = os.getenv('MQTT_SERVICE_HOST', 'mosquitto.local')
 MQTT_SERVICE_PORT = int(os.getenv('MQTT_SERVICE_PORT', 1883))
 MQTT_SERVICE_TOPIC = os.getenv('MQTT_SERVICE_TOPIC', 'home/livingroom')
 MQTT_CLIENT_ID = os.getenv('HOSTNAME', 'dht22-mqtt-service')
 
 logger = logging.getLogger(MQTT_CLIENT_ID)
-
-
-def kill_libgpiod_pulsei():
-    for proc in psutil.process_iter():
-        if proc.name() == "libgpiod_pulsein":
-            proc.kill()
 
 
 if __name__ == "__main__":
@@ -37,49 +31,31 @@ if __name__ == "__main__":
     logger.debug(f"# {MQTT_SERVICE_TOPIC=}")
     logger.debug(f"# {MQTT_CLIENT_ID=}")
     logger.debug("#" * 80)
-    logger.info(f"Waiting a few seconds before initializing DHT22 on pin {DHT22_PIN}...")
-    time.sleep(DHT22_CHECK_EVERY)
+
+    # Initializes DHT22 on given GPIO pin
+    dht22_sensor = adafruit_dht.DHT22(DHT22_PIN)
 
     while True:
 
         try:
-            # Initializes DHT22 on given GPIO pin
-            dht22_sensor = adafruit_dht.DHT22(DHT22_PIN)
-
             # Read from sensor
             temperature = dht22_sensor.temperature
             humidity = dht22_sensor.humidity
-
-            # 100% CPU use of libgpiod_pulsein on Raspberry Pi
-            # https://github.com/adafruit/Adafruit_Blinka/issues/210
-            kill_libgpiod_pulsei()
-            del dht22_sensor
         except RuntimeError as e:
-            kill_libgpiod_pulsei()
-            del dht22_sensor
             logger.error(str(e))
-            # Measure is wrong just after an error
-            # https://github.com/adafruit/Adafruit_CircuitPython_DHT/pull/31
-            # https://github.com/adafruit/Adafruit_Blinka/issues/210#issuecomment-578470762
-            time.sleep(DHT22_CHECK_EVERY)
+            time.sleep(5)
             continue
 
-        # Prepare messages to be published on MQTT
         logger.info(f"[{MQTT_SERVICE_TOPIC}/temperature] --- {temperature}°C ---> [{MQTT_SERVICE_HOST}:{MQTT_SERVICE_PORT}]")
         logger.info(f"[{MQTT_SERVICE_TOPIC}/humidity] ------ {humidity}% ----> [{MQTT_SERVICE_HOST}:{MQTT_SERVICE_PORT}]")
 
-        msgs = [
-            {
-                'topic': f"{MQTT_SERVICE_TOPIC}/temperature",
-                'payload': str(temperature)
-            },
-            {
-                'topic': f"{MQTT_SERVICE_TOPIC}/humidity",
-                'payload': str(humidity)
-            }
-        ]
-
         try:
+            # Prepare messages to be published on MQTT
+            msgs = [{'topic': f"{MQTT_SERVICE_TOPIC}/temperature",
+                     'payload': str(temperature)},
+                    {'topic': f"{MQTT_SERVICE_TOPIC}/humidity",
+                     'payload': str(humidity)}]
+
             # Publish messages on given MQTT broker
             publish.multiple(msgs, hostname=MQTT_SERVICE_HOST, port=MQTT_SERVICE_PORT, client_id=MQTT_CLIENT_ID)
         except Exception:
